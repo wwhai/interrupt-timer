ca661a9fa652ee3ec1f6a409ccd729b510e236a3
wwhai <cnwwhai@gmail.com>
2022-12-23 13:23:59 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,7 @@ volatile bool scheduing = false;
 //
 #define MAX_TASKS 3        // 2 tasks
 #define MAX_TASK_SLICE 100 // 100ms
-#define NEW_TASKS(TASKS...) \
-  MicroTask MicroTasks[] = {TASKS};
+
 static jmp_buf os_context;
 // _JBLEN  23
 typedef unsigned char Byte;
@@ -23,6 +22,13 @@ typedef struct
   Byte RETURN_ADDR;                                                            // return address (PC)
 } CPU_STATE;
 
+asm(".section .text\r\n"
+    ".global AVR_RETI\r\n"
+    "AVR_RETI:\r\n"
+    "\t\tRETI\r\n");
+// 这个函数的神奇之处就是打破了C语言的函数堆栈，牺牲一个空函数换来任务函数的堆栈
+// 也是实时调度的核心。
+extern "C" void AVR_RETI();
 //
 enum MicroTaskState
 {
@@ -48,20 +54,21 @@ typedef struct MicroTask
 
 } MicroTask;
 
+MicroTask MicroTasks[MAX_TASKS] = {};
 //
 // 系统进程表
 //
 static uint8_t TaskId = 0;
-MicroTask NewTask(MicroTaskFunc func)
+volatile unsigned char current_task = 0;
+void NewTask(MicroTaskFunc func)
 {
-  MicroTask task;
-  task.id = TaskId++;
-  task.valid = true;
-  task.state = READY;
-  task.time_slice = MAX_TASK_SLICE;
-  task.switched = false;
-  task.func = func;
-  return task;
+  MicroTasks[TaskId].id = TaskId;
+  MicroTasks[TaskId].valid = true;
+  MicroTasks[TaskId].state = READY;
+  MicroTasks[TaskId].time_slice = MAX_TASK_SLICE;
+  MicroTasks[TaskId].switched = false;
+  MicroTasks[TaskId].func = func;
+  TaskId++;
 }
 //-----------------------------
 // 实现主进程任务的函数
@@ -70,6 +77,8 @@ void f0(void *args)
 {
   Serial.println("##[Task0]## Task0 BEGIN");
 }
+
+// 实现任务的函数
 
 // 实现任务的函数
 void f1(void *args)
@@ -97,54 +106,40 @@ void f2(void *args)
     }
   }
 }
-// 当前任务表数量
-static MicroTask currentTask;
-/// @brief 系统进程表
-/*----------------*/ NEW_TASKS(NewTask(f0), NewTask(f1), NewTask(f2)); /*----------------*/
-uint8_t valid_count = sizeof(MicroTasks) / sizeof(MicroTask);
+
 // 保存上下文
 void TaskYield(void)
 {
   // 如果是第一次表示此时该上下文被保存
   // AddTaskCtx(currentTask.stack)
-  if (!setjmp(MicroTasks[currentTask.id].stack)) // 可以理解为第一次加载任务到Task调度器中
+  if (!setjmp(MicroTasks[current_task].stack)) // 可以理解为第一次加载任务到Task调度器中
   {
     longjmp(os_context, 1); // 加载任务以后, 控制权交给Task管理器
   }
 }
 
-asm(".section .text\r\n"
-    ".global AVR_RETI\r\n"
-    "AVR_RETI:\r\n"
-    "\t\tRETI\r\n");
-// 这个函数的神奇之处就是打破了C语言的函数堆栈，牺牲一个空函数换来任务函数的堆栈
-// 也是实时调度的核心。
-extern "C" void AVR_RETI();
 /// 中断入口
 /*----------------*/ ISR(TIMER1_COMPA_vect) /*----------------*/
 {
-  AVR_RETI();
+  // AVR_RETI();
   Serial.println("# ISR TIMER1 #");
   //-----------------------------------------
-  for (size_t i = 0; i < MAX_TASKS; i++)
+  if (MicroTasks[current_task].valid)
   {
-    if (MicroTasks[currentTask.id].valid)
+    if (MicroTasks[current_task].time_slice > 0) // 还有时间片
     {
-      if (MicroTasks[currentTask.id].time_slice > 0) // 还有时间片
+      if (MicroTasks[current_task].state == RUNNING)
       {
-        if (MicroTasks[currentTask.id].state == RUNNING)
-        {
-          MicroTasks[currentTask.id].time_slice -= 10; // 时间片一直减少
-        }
+        MicroTasks[current_task].time_slice -= 10; // 时间片一直减少
       }
-      else // 时间片用完了
+    }
+    else // 时间片用完了
+    {
+      if (!scheduing) // 全局Flag, 防止中断重复调度
       {
-        if (!scheduing) // 全局Flag, 防止中断重复调度
-        {
-          Serial.println("**** AVR_RETI scheduing BEGIN.");
-          scheduing = true; // 开始调度
-          TaskYield();
-        }
+        Serial.println("**** AVR_RETI scheduing BEGIN.");
+        scheduing = true; // 开始调度
+        TaskYield();
       }
     }
   }
@@ -156,31 +151,31 @@ extern "C" void AVR_RETI();
 void RunTask()
 {
 
-  for (currentTask.id = 0; currentTask.id < MAX_TASKS; currentTask.id++)
+  for (current_task = 0; current_task < MAX_TASKS; current_task++)
   {
     if (setjmp(os_context)) // 理解为加载了个进程进来进行调度
     {
       scheduing = false;
       continue; // TODO 问题出在这里了， 导致上下文没有恢复成功
     }
-    if (MicroTasks[currentTask.id].valid)
+    if (MicroTasks[current_task].valid)
     {
-      switch (MicroTasks[currentTask.id].state)
+      switch (MicroTasks[current_task].state)
       {
       case READY:
       {
         Serial.print("Current RunTask... BEGIN:");
         Serial.print("[");
-        Serial.print(currentTask.id);
+        Serial.print(current_task);
         Serial.print("]\n\r");
-        MicroTasks[currentTask.id].state = RUNNING; // 开始执行
-        MicroTasks[currentTask.id].func(0);         // 执行
-        MicroTasks[currentTask.id].state = STOP;    // 执行结束, 任务只有到这里才算是结束
-        MicroTasks[currentTask.id].valid = false;   // 执行完了就把任务给清了
-        valid_count--;
+        MicroTasks[current_task].state = RUNNING; // 开始执行
+        MicroTasks[current_task].func(0);         // 执行
+        MicroTasks[current_task].state = STOP;    // 执行结束, 任务只有到这里才算是结束
+        MicroTasks[current_task].valid = false;   // 执行完了就把任务给清了
+        MicroTasks[current_task].time_slice = 0;
         Serial.print("Current RunTask... END:");
         Serial.print("[");
-        Serial.print(currentTask.id);
+        Serial.print(current_task);
         Serial.print("]\n\r");
       }
       break;
@@ -189,21 +184,21 @@ void RunTask()
       //-----------------------------------------------------------------
       case RUNNING: // 恢复运行, 实际上其状态还是RUNNING, 再次给他时间片
         Serial.println("Recover--------------------------");
-        MicroTasks[currentTask.id].time_slice = MAX_TASK_SLICE;
-        longjmp(MicroTasks[currentTask.id].stack, 1);
+        MicroTasks[current_task].time_slice = MAX_TASK_SLICE;
+        longjmp(MicroTasks[current_task].stack, 1);
         break;
       case BLOCK: // Sleep() 阻塞任务
-        MicroTasks[currentTask.id].time--;
-        if (MicroTasks[currentTask.id].time == 0)
+        MicroTasks[current_task].time--;
+        if (MicroTasks[current_task].time == 0)
         {
-          if (MicroTasks[currentTask.id].valid)
+          if (MicroTasks[current_task].valid)
           {
-            MicroTasks[currentTask.id].state = READY;
+            MicroTasks[current_task].state = READY;
           }
         }
         break;
       case STOP:
-        MicroTasks[currentTask.id].valid = false;
+        MicroTasks[current_task].valid = false;
         break;
       default:
         TaskYield();
@@ -225,6 +220,9 @@ void setup()
   TIMSK1 = (1 << OCIE1A);
   sei();
   Serial.println("TIMER1 Setup Finished.");
+  NewTask(f0);
+  NewTask(f1);
+  NewTask(f2);
 }
 void loop()
 {
